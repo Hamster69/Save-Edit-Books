4332a2f4e6e89d8320abc2b936ebbf0bb9aef092
Ariel Aguilar <ariel.aguilar.ricaldez@gmail.com>
2019-01-26 18:55:09 -0400

    Added custom form to save and edit books, it's not complete yet

--- src/app/Controllers/book-list.controller.js
+++ src/app/Controllers/book-list.controller.js
@@ -3,9 +3,9 @@
     angular
         .module('app')
         .controller('BookListController', BookListController);
-        BookListController.$inject = ['BookService'];
+        BookListController.$inject = [$mdDialog, 'BookService'];
     
-        function BookListController(BookService) {
+        function BookListController(BookService, $mdDialog) {
             var vm = this;
             
             vm.bookList = () => {
@@ -21,6 +21,26 @@
                 edition: null,
                 authors: []
             };
+
+            vm.showPrompt = function(ev) {
+                // Appending dialog to document.body to cover sidenav in docs app
+                var confirm = $mdDialog.prompt()
+                  .title('Salvar / Editar')
+                  .textContent('Bowser is a common name.')
+                  .placeholder('Dog name')
+                  .ariaLabel('Dog name')
+                  .initialValue('Buddy')
+                  .targetEvent(ev)
+                  .required(true)
+                  .ok('Okay!')
+                  .cancel('I\'m a cat person');
             
+                $mdDialog.show(confirm).then(function(result) {
+                  vm.status = 'You decided to name your dog ' + result + '.';
+                }, function() {
+                  vm.status = 'You didn\'t name your dog.';
+                });
+              };
+
         }
 })();
